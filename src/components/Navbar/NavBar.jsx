192591a6f8627245f70587ace577f76c14c68c46
sakibhasansufi <sakibhasansufi@gmail.com>
2024-08-16 22:54:57 +0600

    navbar is set up

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,28 +1,11 @@
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, Link, Button, DropdownItem, Dropdown, DropdownTrigger, Avatar, DropdownMenu } from "@nextui-org/react";
-import React from "react";
+import { Navbar, NavbarBrand, NavbarContent, Link, Button, DropdownItem, Dropdown, DropdownTrigger, Avatar, DropdownMenu } from "@nextui-org/react";
 
 const NavBar = () => {
-    const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
-    const menuItems = [
-        "Profile",
-        "Dashboard",
-        "Activity",
-        "Analytics",
-        "System",
-        "Deployments",
-        "My Settings",
-        "Team Settings",
-        "Help & Feedback",
-        "Log Out",
-    ];
     return (
-        <Navbar onMenuOpenChange={setIsMenuOpen}>
+        <Navbar >
+            
             <NavbarContent>
-                <NavbarMenuToggle
-                    aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-                    className="sm:hidden"
-                />
                 <NavbarBrand>
 
                     <p className="font-bold text-inherit">ACME</p>
@@ -31,35 +14,15 @@ const NavBar = () => {
 
 
 
-            <NavbarContent  className="hidden md:block relative mt-8 md:ml-[85px] lg:ml-32">
+            <NavbarContent className=" relative mt-2 md:mt-3 ml-16 md:ml-[85px] lg:ml-32">
                 <Button as={Link} color="success" href="#" variant="flat">
                     Log In
                 </Button>
 
-                <Button as={Link} color="primary" href="#" variant="flat" className="ml-4">
+                <Button as={Link} color="primary" href="#" variant="flat" className="md:ml-4">
                     Sign Up
                 </Button>
             </NavbarContent>
-
-
-
-            <NavbarMenu>
-                {menuItems.map((item, index) => (
-                    <NavbarMenuItem key={`${item}-${index}`}>
-                        <Link
-                            color={
-                                index === 2 ? "primary" : index === menuItems.length - 1 ? "danger" : "foreground"
-                            }
-                            className="w-full"
-                            href="#"
-                            size="lg"
-                        >
-                            {item}
-                        </Link>
-                    </NavbarMenuItem>
-                ))}
-            </NavbarMenu>
-
 
             <NavbarContent as="div" justify="end">
                 <Dropdown placement="bottom-end">
