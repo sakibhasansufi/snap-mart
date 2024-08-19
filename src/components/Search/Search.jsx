0472bcd5b460bd4c42c589748db5f078b6bbf9dd
sakibhasansufi <sakibhasansufi@gmail.com>
2024-08-19 22:26:30 +0600

    search method is set up

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -12,9 +12,6 @@ const Search = ({ setSearch }) => {
                     placeholder="Search"
                     onChange={({ currentTarget: input }) => setSearch(input.value)} />
 
-                    {/* <input type="text" className="px-4 py-2 text-gray-700 bg-white border rounded-md sm:mx-2 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40  md:w-1/2" placeholder="Search"
-                        onChange={({ currentTarget: input }) => setSearch(input.value)}
-                    /> */}
 
             </section>
         </div>
